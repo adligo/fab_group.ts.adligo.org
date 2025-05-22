5a22b849711e4249de3d11c1cfd559e4072ee72e
Adligo Inc <scott@adligo.com>
2025-05-21 22:25:35 -0500

    Hmm trying to pass in the shell to it.

--- buildSrc/setup.cjs
+++ buildSrc/setup.cjs
@@ -18,14 +18,27 @@ const [run1, run2, run3] = require('./runFuns.cjs');
   * See the License for the specific language governing permissions and
   * limitations under the License.
   */
-//run('pwd',[], { cwd: 'log2.ts.adligo.org'})
-run3('slink',['--debug'], { cwd: 'log2.ts.adligo.org'})
-run3('slink',[], { cwd: 'log2_tests.ts.adligo.org'})
-run3('slink',[], { cwd: 'tests4ts.ts.adligo.org'})
-run3('slink',[], { cwd: 'junitXml.tests4j.ts.adligo.org'})
-run3('slink',[], { cwd: 'junitXml_tests.tests4j.ts.adligo.org'})
-run3('slink',[], { cwd: 'tests4jXml.ts.adligo.org'})
-run3('slink',[], { cwd: 'tests4jXml_tests.ts.adligo.org'})
+//run('pwd',[], getOpts('log2.ts.adligo.org'})
+function getOpts(dir) {
+	var obj = {};
+	if (dir != undefined) {
+		obj = new Object();
+		obj.cwd = dir;
+	}
+	if (process.env.USHELL != undefined) {
+		console.log("USHELL is " + process.env.USHELL);
+		obj.shell = process.env.USHELL
+	}
+	return obj;
+}
+
+run3('slink',['--debug'], getOpts('log2.ts.adligo.org'))
+run3('slink',[], getOpts('log2_tests.ts.adligo.org'))
+run3('slink',[], getOpts('tests4ts.ts.adligo.org'))
+run3('slink',[], getOpts('junitXml.tests4j.ts.adligo.org'))
+run3('slink',[], getOpts('junitXml_tests.tests4j.ts.adligo.org'))
+run3('slink',[], getOpts('tests4jXml.ts.adligo.org'))
+run3('slink',[], getOpts('tests4jXml_tests.ts.adligo.org'))
 
 // do all the npm i last
-run3('npm',['i'], { cwd: 'tests4ts.ts.adligo.org'})
+run3('npm',['i'], getOpts('tests4ts.ts.adligo.org'))
