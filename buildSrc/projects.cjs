7723759f5c8cba8303674b27d12e5905112eb692
Adligo Inc <scott@adligo.com>
2025-06-13 21:24:23 -0500

    Breaking the world for a minute

--- buildSrc/projects.cjs
+++ buildSrc/projects.cjs
@@ -40,12 +40,14 @@ new Project('i_cli.ts.adligo.org', true),new Project('i_fab_core.ts.adligo.org',
 // end of interface projects
 // TODO put code in the following projects that are turned off with comment lines like;
 // true, true), or true),
+new Project('type-guards.ts.adligo.org',true),
 new Project('cli.ts.adligo.org'), // true),
 new Project('log2.ts.adligo.org', true),
 //build tests4ts before any testing library
 new Project('tests4ts.ts.adligo.org', true), 
 //build the JUnit xml generator before any of the testing libraries
 new Project('junit-xml-tests4j.ts.adligo.org', true), 
+new Project('type-guards_tests.ts.adligo.org', true, true), 
 new Project('cli_tests.ts.adligo.org'), // true),
 new Project('log2_tests.ts.adligo.org', true, true),// true),
 new Project('tests4ts_tests.ts.adligo.org'), //, true, true
